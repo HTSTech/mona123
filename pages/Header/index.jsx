f5a0ae93e239cff9794ade089287bcec9f92bc14
HTSpankaj <pankaj.agade@htstechsolutions.com>
2022-09-15 15:58:11 +0530

    feat: Added OUTPUT.md programmatically

---
+++ pages/Header/index.jsx
@@ -0,0 +1,40 @@
+
+  import { useEffect } from 'react';
+  import axios from 'axios';
+  import Link from 'next/link';
+  
+  export default function Header() {
+  
+    useEffect(() => {
+      getData();
+    }, [])
+   
+    async function getData() {
+      
+      axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/9c7e3b24-fec3-4e4e-8046-e7a3fc02b604/mona123/Header/index.html").then(res=>{
+        console.log("res=> ",res);
+        document.getElementById("HeaderID").innerHTML = res.data;
+        
+        //* css
+        axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/9c7e3b24-fec3-4e4e-8046-e7a3fc02b604/mona123/Header/style.css").then(CssRes=>{
+          console.log("CssRes", CssRes);
+          let createCssElement = document.createElement("style");
+          // createCssElement.nodeValue = CssRes.data;
+          createCssElement.appendChild(document.createTextNode(CssRes.data));
+          
+          document.getElementById("HeaderID").appendChild(createCssElement)
+          axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/9c7e3b24-fec3-4e4e-8046-e7a3fc02b604/mona123/Header/script.js").then(JsRes=>{
+            console.log("JsRes", JsRes);
+            eval(JsRes.data)
+          })
+        })
+      })
+  
+    }
+    
+  
+    return (
+      <div id='HeaderID'>
+      </div>
+    )
+  }
